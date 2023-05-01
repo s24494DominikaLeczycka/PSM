7777c7b51292b79d0e9c474dac244d3599e966d2
s24494DominikaLeczycka <s24494@pjwstkk.edu.pl>
2023-05-01 18:10:06 +0200

    Adding more interface

--- PSM8/main.py
+++ PSM8/main.py
@@ -4,7 +4,9 @@ import matplotlib.animation as animation
 from scipy.signal import convolve2d
 from matplotlib.colors import ListedColormap
 from matplotlib.patches import Patch
-
+from matplotlib.widgets import Button
+import tkinter as tk
+from tkinter import simpledialog
 
 def count_neighbors(grid):
     kernel = np.array([[1, 1, 1],
@@ -34,9 +36,42 @@ def game_of_life(grid, rules):
     colors[(old_grid == 1) & (new_grid == 0) & (neighbors > 3)] = 4  # Orange
 
     return new_grid, old_grid, colors
-def on_key(event, canvas):
+def restart_animation(fig, grid, grid_size):
+    grid[:] = np.random.choice([0, 1], grid_size * grid_size, p=[0.8, 0.2]).reshape(grid_size, grid_size)
+    fig.canvas.draw_idle()
+
+def change_rules(event, rules):
+    def submit():
+        survive_str = e_survive.get()
+        birth_str = e_birth.get()
+        survive = [int(x) for x in survive_str.split(",")]
+        birth = [int(x) for x in birth_str.split(",")]
+        rules[0] = survive
+        rules[1] = birth
+        input_dialog.destroy()
+
+    input_dialog = tk.Tk()
+    input_dialog.title("Update Rules")
+
+    tk.Label(input_dialog, text="Survive rules (separated by a comma): ").grid(row=0, column=0)
+    e_survive = tk.Entry(input_dialog)
+    e_survive.grid(row=0, column=1)
+
+    tk.Label(input_dialog, text="Birth rules (separated by a comma): ").grid(row=1, column=0)
+    e_birth = tk.Entry(input_dialog)
+    e_birth.grid(row=1, column=1)
+
+    tk.Button(input_dialog, text="Submit", command=submit).grid(row=2, column=1)
+
+    input_dialog.mainloop()
+
+def on_key(event, fig, grid, grid_size, rules):
     if event.key == 'ctrl+c':
         plt.close()
+    elif event.key == 'r':
+        restart_animation(fig, grid, grid_size)
+    elif event.key == 'u':
+        change_rules(event, rules)
 
 def update(frame, grid, img_bw, img_colored, rules):
     grid[:], old_grid, colors = game_of_life(grid, rules)
@@ -49,11 +84,11 @@ def main():
     steps = 100
     survive = [2, 3]
     birth = [3]
-    rules = (survive, birth)
+    rules = [survive, birth]  # Use a list instead of a tuple
 
     grid = np.random.choice([0, 1], grid_size * grid_size, p=[0.8, 0.2]).reshape(grid_size, grid_size)
 
-    fig, (ax_bw, ax_colored) = plt.subplots(1, 2, figsize=(20, 7))
+    fig, (ax_bw, ax_colored) = plt.subplots(1, 2, figsize=(21, 7))
 
     img_bw = ax_bw.imshow(grid, cmap='binary', interpolation="nearest", vmin=0, vmax=1)
     _, _, colors = game_of_life(grid, rules)
@@ -79,7 +114,18 @@ def main():
 
     # Bind keypress event to the on_key function
     canvas = fig.canvas
-    canvas.mpl_connect('key_press_event', lambda event: on_key(event, canvas))
+    canvas.mpl_connect('key_press_event', lambda event: on_key(event, fig, grid, grid_size, rules))
+
+    fig.subplots_adjust(bottom=0.2)  # Add space at the bottom for buttons
+
+    # Add buttons for restarting the animation and updating the rules
+    restart_button_ax = plt.axes([0.35, 0.05, 0.1, 0.075])
+    restart_button = Button(restart_button_ax, 'Restart')
+    restart_button.on_clicked(lambda event: restart_animation(event, grid, grid_size))
+
+    update_rules_button_ax = plt.axes([0.55, 0.05, 0.1, 0.075])
+    update_rules_button = Button(update_rules_button_ax, 'Update Rules')
+    update_rules_button.on_clicked(lambda event: change_rules(event, rules))
 
     plt.show()
 
